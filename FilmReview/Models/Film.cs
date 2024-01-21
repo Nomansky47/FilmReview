a1c7ff66c02aea3ab198df9e0d79756a4d3095ca
Nomansky <kita31200@gmail.com>
2024-01-21 16:43:29 +0300

    Initial commit

---
+++ FilmReview/Models/Film.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmReview.Models
+{
+    public class Film
+    {
+        [Key]
+        public int FilmID { get; set; }
+        [Required]
+        public string FilmName { get; set; }
+        [Required]
+        public string FilmTags { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public float Rating { get; set; } = 0;
+    }
+}
---
+++ FilmReview/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmReview.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        public string UserLogin { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public bool isAdmin { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Patronymic { get; set; }
+    }
+}
