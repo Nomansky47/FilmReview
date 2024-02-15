de2bc3cd10bd1aaa95a1f07756176ddb588c6abc
Nomansky47 <kita31200@gmail.com>
2024-02-15 18:00:06 +0300

    registration final

--- FilmReview/Controllers/AuthorizationController.cs
+++ FilmReview/Controllers/AuthorizationController.cs
@@ -10,30 +10,6 @@ namespace FilmReview.Controllers
 {
     public class AuthorizationController : Controller
     {
-        private readonly MyContext _context;
-        public AuthorizationController(MyContext context)
-        {
-            _context = context;
-        }
-        [Route("Register/{NickName}/{UserLogin}/{Password}/{Surname}/{Name}/{Patronymic?}")]
-        public async Task<IActionResult> Register(string NickName,string UserLogin,string Password,string Surname,string Name,string? Patronymic)
-        {
-            Users user = new Users();
-            user.NickName = NickName;
-            user.UserLogin = UserLogin;
-            user.Password = Password;
-            user.Surname = Surname;
-            user.Name = Name;
-            user.Patronymic = Patronymic;
-            if (_context.Users.FirstOrDefault(p => p.UserLogin == user.UserLogin) == null)
-            {
-                user.isAdmin = false;
-                _context.Users.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("HomePage", "Home");
-            }
-            return View("HomePage","Not");
-        }
         [Route("Auth/{userid}/{isAdmin}")]
           public IActionResult Auth(string userid,bool isAdmin)
           {
