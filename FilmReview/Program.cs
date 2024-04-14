ad475a4e47ddfcfe2d78e5331cfee9dd79e9cba1
Nomansky47 <kita31200@gmail.com>
2024-04-14 16:35:35 +0300

    best fix ever

--- FilmReview/Program.cs
+++ FilmReview/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddSession();
+builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazorise(options =>{options.Immediate = true;}).AddBootstrapProviders().AddFontAwesomeIcons();
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 //DbInitializer.AddMySqlContext<MyContext>(builder.Services, connectionString);
@@ -24,7 +25,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
